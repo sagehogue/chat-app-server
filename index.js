6f46b8162dc001f07c7e91760159d4ad4f168696
Sage Hogue, overlord <sagehogue@gmail.com>
2020-05-16 15:15:31 -0700

    Merge conflicts (hopefully) resolved through incorporation

--- index.js
+++ index.js
@@ -12,12 +12,52 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-// cross-origin-resource-sharing
+// Initializing firebase
+
+const serviceAccount = require("./API_KEY.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://chat-app-c2d82.firebaseio.com"
+});
+
 app.use(cors());
 app.use(router);
 
-// Main activity handler
-io.on("connect", (socket) => {
+db = admin.firestore();
+const roomsRef = db.collection("/rooms");
+const testRef = roomsRef.doc("test");
+
+// Testing firebase functionality
+
+// This is how you set a message - specifically how to update an array of objects in firebase with a new entry
+let setTestMessages = testRef.update({
+  messageHistory: admin.firestore.FieldValue.arrayUnion({
+    name: "David Lucas",
+    message: "your toilet seat is probably shaped like a dildo",
+    time: new Date().toLocaleString()
+  })
+});
+
+// This is how to log all data from a collection
+
+let getDoc = roomsRef
+  .get()
+  .then(col => {
+    col.forEach(doc => {
+      if (!doc.exists) {
+        console.log("No such document!");
+      } else {
+        console.log(`Document Name: ${doc.id}\n`, "Document data:", doc.data());
+      }
+    });
+  })
+  .catch(err => {
+    console.log("Error getting document", err);
+  });
+
+// socket.io event listeners
+
+io.on("connect", socket => {
   socket.on("join", ({ name, room }, callback) => {
     let messageHistory;
     // Get Messages
@@ -50,10 +90,23 @@ io.on("connect", (socket) => {
           .emit("message", { user: "admin", text: `${user.name} has joined!` });
       }
     );
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-    io.to(user.room).emit("roomData", {
-      room: user.room,
-      users: getUsersInRoom(user.room),
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to room ${user.room}.`
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
     });
 
     callback();
@@ -73,11 +126,11 @@ io.on("connect", (socket) => {
     if (user) {
       io.to(user.room).emit("message", {
         user: "Admin",
-        text: `${user.name} has left.`,
+        text: `${user.name} has left.`
       });
       io.to(user.room).emit("roomData", {
         room: user.room,
-        users: getUsersInRoom(user.room),
+        users: getUsersInRoom(user.room)
       });
     }
   });
