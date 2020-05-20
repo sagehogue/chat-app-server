8745123bf3a00dcd7ac1e3adb0e04f7d6d3fa784
Sage Hogue, overlord <sagehogue@gmail.com>
2020-05-19 17:51:41 -0700

    normalizing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const cors = require("cors");
+const admin = require("firebase-admin");
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 const { addMessageToRoom, getMessageHistory } = require("./messages");
@@ -17,7 +18,7 @@ const io = socketio(server);
 const serviceAccount = require("./API_KEY.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://chat-app-c2d82.firebaseio.com"
+  databaseURL: "https://chat-app-c2d82.firebaseio.com",
 });
 
 app.use(cors());
@@ -34,16 +35,16 @@ let setTestMessages = testRef.update({
   messageHistory: admin.firestore.FieldValue.arrayUnion({
     name: "David Lucas",
     message: "your toilet seat is probably shaped like a dildo",
-    time: new Date().toLocaleString()
-  })
+    time: new Date().toLocaleString(),
+  }),
 });
 
 // This is how to log all data from a collection
 
 let getDoc = roomsRef
   .get()
-  .then(col => {
-    col.forEach(doc => {
+  .then((col) => {
+    col.forEach((doc) => {
       if (!doc.exists) {
         console.log("No such document!");
       } else {
@@ -51,45 +52,43 @@ let getDoc = roomsRef
       }
     });
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Error getting document", err);
   });
 
 // socket.io event listeners
 
-io.on("connect", socket => {
+io.on("connect", (socket) => {
   socket.on("join", ({ name, room }, callback) => {
     let messageHistory;
     // Get Messages
-    const { error, messages } = getMessageHistory(db, room).then(
-      (err, msgs) => {
-        // Add user to user list
-        const { userError, user } = ({ error, user } = addUser({
-          id: socket.id,
-          name,
-          room,
-        }));
-        // Msg history is blank/room not found
-        if (err) messageHistory = [];
-        // Room/messagehistory found
-        else messageHistory = msgs;
-        // Error occured
-        if (userError) return callback(error);
-        // Connect user
-        socket.join(user.room);
-        // Send welcome message to user
-        socket.emit("message", {
-          user: "admin",
-          text: `${user.name}, welcome to room ${user.room}.`,
-        });
-        // Send messageHistory to user
-        socket.emit("messageHistory", messageHistory);
-        // Send announcement to room that user has joined.
-        socket.broadcast
-          .to(user.room)
-          .emit("message", { user: "admin", text: `${user.name} has joined!` });
-      }
-    );
+    const getMessage = getMessageHistory(db, room).then((err, msgs) => {
+      // Add user to user list
+      const { error, user } = ({ error, user } = addUser({
+        id: socket.id,
+        name,
+        room,
+      }));
+      // Msg history is blank/room not found
+      if (err) messageHistory = [];
+      // Room/messagehistory found
+      else messageHistory = msgs;
+      // Error occured
+      if (error) return callback(error);
+      // Connect user
+      socket.join(user.room);
+      // Send welcome message to user
+      socket.emit("message", {
+        user: "admin",
+        text: `${user.name}, welcome to room ${user.room}.`,
+      });
+      // Send messageHistory to user
+      socket.emit("messageHistory", messageHistory);
+      // Send announcement to room that user has joined.
+      socket.broadcast
+        .to(user.room)
+        .emit("message", { user: "admin", text: `${user.name} has joined!` });
+    });
     const { error, user } = addUser({ id: socket.id, name, room });
 
     if (error) return callback(error);
@@ -98,7 +97,7 @@ io.on("connect", socket => {
 
     socket.emit("message", {
       user: "admin",
-      text: `${user.name}, welcome to room ${user.room}.`
+      text: `${user.name}, welcome to room ${user.room}.`,
     });
     socket.broadcast
       .to(user.room)
@@ -106,7 +105,7 @@ io.on("connect", socket => {
 
     io.to(user.room).emit("roomData", {
       room: user.room,
-      users: getUsersInRoom(user.room)
+      users: getUsersInRoom(user.room),
     });
 
     callback();
@@ -126,16 +125,16 @@ io.on("connect", socket => {
     if (user) {
       io.to(user.room).emit("message", {
         user: "Admin",
-        text: `${user.name} has left.`
+        text: `${user.name} has left.`,
       });
       io.to(user.room).emit("roomData", {
         room: user.room,
-        users: getUsersInRoom(user.room)
+        users: getUsersInRoom(user.room),
       });
     }
   });
 });
-
+console.log(process.env.PORT || 5000);
 server.listen(process.env.PORT || 5000, () =>
   console.log(`Server has started.`)
 );
